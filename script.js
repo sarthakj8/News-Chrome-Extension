08dc648b340ccd096fa725c64b9a5e1c6741616c
Sarthak Jain <92021691+sarthakj8@users.noreply.github.com>
2021-11-24 19:23:49 +0530

    version 2.0 of news extension

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("linktobusiness").addEventListener("click", linktobusiness);
+    document.getElementById("linktosports").addEventListener("click", linktosports);
+    document.getElementById("linktohealth").addEventListener("click", linktohealth);
+    document.getElementById("linktoscience").addEventListener("click", linktoscience);
+    document.getElementById("linktotechnology").addEventListener("click", linktotechnology);
+    document.getElementById("linktoentertainment").addEventListener("click", linktoentertainment);
+  });
+function linktobusiness(){
 let url='https://saurav.tech/NewsAPI/top-headlines/category/business/in.json';
-
-
 
     fetch(url)
         .then(response =>{
@@ -24,53 +31,183 @@ let url='https://saurav.tech/NewsAPI/top-headlines/category/business/in.json';
             const newsElement=document.getElementById('newsElement')
             newsElement.innerHTML=`
                 <div>
-                    <ul>
-                        <li><a href="#home"><button>HOME</button></a></li>
-                        <li><a href="#sports"><button onclick="cata=url1">SPORTS</button></a></li>
-                        <li><a href="#entertainment"><button  onclick="cata=url2">ENTERTAINMENT</button></a></li>
-                        <li><a href="#technology"><button>TECHNOLOGY</button></a></li>
-                        <li><a href="#science"><button>SCIENCE</button></a></li>
-                        <li><a href="#health"><button>HEALTH</button></a></li>
-                        <li><a href="#business"><button>BUSINESS</button></a></li>
-                    </ul>
+                    <h1>BUSINESS  SECTION</h1>            
                 </div>
                 <div>
-                    <br><a name="home"></a>
-                    <h1>WELCOME TO SPORTS SECTION</h1>This news extension will keep you updated with latest event around you            
-                </div>
-                <div>
-                    <br><a name="sports"></a>${newsTitle1}
+                    ${newsTitle1}
                 </div>
             `;
         })
-    
+        
+}
 
-    // fetch(url2)
-    //     .then(response =>{
-    //         if(!response.ok){
-    //             throw Error('error');
-    //             }
-    //             return response.json();
-    //         })
-    //     .then(data =>{
-    //         console.log(data.articles);
-    //         const newsTitle2=data.articles
-    //             .map(article=>{return`
-    //                 <div>
-    //                     <p><B>${article.title}</B><br><img src="${article.urlToImage}" alt="IMAGE NOT AVAILABLE"></p><br>
-    //                 </div><br>
-    //                 `;
-    //             })
-    //             .join('');
-    
-    //         const newsElement=document.getElementById('newsElement')
-    //         newsElement.innerHTML=`
-    //             <div>
-    //                 <br><a name="entertainment"></a>${newsTitle2}
-    //             </div>`;
-    //     })
-    //     .catch(error=>{
-    //         console.log(error)
-    //     });
-    
+function linktosports(){
+    let url='https://saurav.tech/NewsAPI/top-headlines/category/sports/in.json';
 
+    fetch(url)
+        .then(response =>{
+            if(!response.ok){
+                throw Error('error');
+            }
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data.articles);
+            const newsTitle1=data.articles
+                .map(article=>{
+                    return`
+                    <div id="news" class="clearfix">
+                        <img src="${article.urlToImage}" alt="IMAGE NOT AVAILABLE"><br><p><B>${article.title}</B></p><br>
+                    </div><br>
+                    `;
+                })
+                .join('');
+
+            const newsElement=document.getElementById('newsElement')
+            newsElement.innerHTML=`
+                <div>
+                    <h1>SPORTS  SECTION</h1>       
+                </div>
+                <div>
+                    ${newsTitle1}
+                </div>
+            `;
+        })
+}
+
+function linktoentertainment(){
+    let url='https://saurav.tech/NewsAPI/top-headlines/category/entertainment/in.json';
+
+    fetch(url)
+        .then(response =>{
+            if(!response.ok){
+                throw Error('error');
+            }
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data.articles);
+            const newsTitle1=data.articles
+                .map(article=>{
+                    return`
+                    <div id="news" class="clearfix">
+                        <img src="${article.urlToImage}" alt="IMAGE NOT AVAILABLE"><br><p><B>${article.title}</B></p><br>
+                    </div><br>
+                    `;
+                })
+                .join('');
+
+            const newsElement=document.getElementById('newsElement')
+            newsElement.innerHTML=`
+                <div>
+                    <h1>ENTERTAINMENT  SECTION</h1>            
+                </div>
+                <div>
+                    ${newsTitle1}
+                </div>
+            `;
+        })
+}
+
+function linktoscience(){
+    let url='https://saurav.tech/NewsAPI/top-headlines/category/science/in.json';
+
+    fetch(url)
+        .then(response =>{
+            if(!response.ok){
+                throw Error('error');
+            }
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data.articles);
+            const newsTitle1=data.articles
+                .map(article=>{
+                    return`
+                    <div id="news" class="clearfix">
+                        <img src="${article.urlToImage}" alt="IMAGE NOT AVAILABLE"><br><p><B>${article.title}</B></p><br>
+                    </div><br>
+                    `;
+                })
+                .join('');
+
+            const newsElement=document.getElementById('newsElement')
+            newsElement.innerHTML=`
+                <div>
+                    <h1>SCIENCE  SECTION</h1>            
+                </div>
+                <div>
+                    ${newsTitle1}
+                </div>
+            `;
+        })
+}
+
+function linktohealth(){
+    let url='https://saurav.tech/NewsAPI/top-headlines/category/health/in.json';
+
+    fetch(url)
+        .then(response =>{
+            if(!response.ok){
+                throw Error('error');
+            }
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data.articles);
+            const newsTitle1=data.articles
+                .map(article=>{
+                    return`
+                    <div id="news" class="clearfix">
+                        <img src="${article.urlToImage}" alt="IMAGE NOT AVAILABLE"><br><p><B>${article.title}</B></p><br>
+                    </div><br>
+                    `;
+                })
+                .join('');
+
+            const newsElement=document.getElementById('newsElement')
+            newsElement.innerHTML=`
+                <div>
+                    
+                    <h1>HEALTH  SECTION</h1>            
+                </div>
+                <div>
+                    ${newsTitle1}
+                </div>
+            `;
+        })
+}
+
+function linktotechnology(){
+    let url='https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json';
+
+    fetch(url)
+        .then(response =>{
+            if(!response.ok){
+                throw Error('error');
+            }
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data.articles);
+            const newsTitle1=data.articles
+                .map(article=>{
+                    return`
+                    <div id="news" class="clearfix">
+                        <img src="${article.urlToImage}" alt="IMAGE NOT AVAILABLE"><br><p><B>${article.title}</B></p><br>
+                    </div><br>
+                    `;
+                })
+                .join('');
+
+            const newsElement=document.getElementById('newsElement')
+            newsElement.innerHTML=`
+                <div>
+                    <h1>TECHNOLOGY  SECTION</h1>            
+                </div>
+                <div>
+                    ${newsTitle1}
+                </div>
+            `;
+        })
+}
